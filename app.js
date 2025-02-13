c3cc3438b9183468dae2e7cba513d9694a04490a
Gergo <kapusi.gergo.levente@students.jedlik.eu>
2025-02-13 07:49:56 +0100

    SavePost Button +logo poz fix

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
-function newPost() {
-    
-    let window = document.getElementById("post-window")
-    let postmenu = document.getElementById("postmenu")
-    if (window.classList.contains("footnote-open")) {
-        window.classList.remove("footnote-open")
-        postmenu.classList.remove("shown")
-        return
-    }
-    window.classList.add("footnote-open")
-    postmenu.classList.add("shown")
-    postmenu.classList.add("overflow-on")
+function OpenPostMenu() {
+  let window = document.getElementById("post-window");
+  let postmenu = document.getElementById("postmenu");
+  if (window.classList.contains("footnote-open")) {
+    window.classList.remove("footnote-open");
+    postmenu.classList.remove("shown");
+    return;
+  }
+  window.classList.add("footnote-open");
+  postmenu.classList.add("shown");
+  postmenu.classList.add("overflow-on");
 }
