45270adbd85698682022bf58fd38172dc772aebf
novakitamas <novaki.tams8@gmail.com>
2025-02-20 12:34:05 +0100

    progressbars,labels

--- app.js
+++ app.js
@@ -11,3 +11,27 @@ function newPost() {
     postmenu.classList.add("shown")
     postmenu.classList.add("overflow-on")
 }
+
+function setProgress(value) {
+    if (value < 1 || value > 10) return;
+    const progressBar = document.querySelector('.progress-barar');
+    const progressText = document.querySelector('.progress-textar');
+    
+    const percentage = (value / 10) * 100;
+    progressBar.style.width = percentage + "%";
+    progressText.textContent = `${value}/10`;
+}
+
+setProgress(5);
+
+function setProgressall (value) {
+    if (value < 1 || value > 40) return;
+    const progressBar = document.querySelector('.progress-barall');
+    const progressText = document.querySelector('.progress-textall');
+    
+    const percentage = (value / 40) * 100;
+    progressBar.style.width = percentage + "%";
+    progressText.textContent = `${value}/40`;
+}
+
+setProgressall(10);
